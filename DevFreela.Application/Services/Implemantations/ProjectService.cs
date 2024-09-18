865359b90a60b496017288b211baa1730a21a026
Danilo Rodrigues de Oliveira <danilo.rodrigues38@gmail.com>
2024-09-18 17:46:06 -0300

    Criada a camada da Application e iniciado as implementações.

---
+++ DevFreela.Application/Services/Implemantations/ProjectService.cs
@@ -0,0 +1,49 @@
+﻿using DevFreela.Application.InputModels;
+using DevFreela.Application.Services.Interfaces;
+using DevFreela.Application.ViewModels;
+
+namespace DevFreela.Application.Services.Implemantations
+{
+    public class ProjectService : IProjectService
+    {
+        public int Create ( NewProjectInputModel inputModel )
+        {
+            throw new NotImplementedException ( );
+        }
+
+        public void CreateComment ( CreateCommentInputModel inputModel )
+        {
+            throw new NotImplementedException ( );
+        }
+
+        public void Delelte ( int id )
+        {
+            throw new NotImplementedException ( );
+        }
+
+        public void Finish ( int id )
+        {
+            throw new NotImplementedException ( );
+        }
+
+        public List<ProjectViewModel> GetAll ( string query )
+        {
+            throw new NotImplementedException ( );
+        }
+
+        public ProjectDetailsViewModel GetById ( int id )
+        {
+            throw new NotImplementedException ( );
+        }
+
+        public void Start ( int id )
+        {
+            throw new NotImplementedException ( );
+        }
+
+        public void Update ( UpdateProjectInputModel inputModel )
+        {
+            throw new NotImplementedException ( );
+        }
+    }
+}
