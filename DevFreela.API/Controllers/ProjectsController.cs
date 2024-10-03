c052b567afb83031c5c9bfe6bace85271dde3849
Danilo Rodrigues de Oliveira <danilo.rodrigues38@gmail.com>
2024-10-03 14:43:01 -0300

    Finalizada a aula de arquitetura limpa com a refatoração do camada API.

--- DevFreela.API/Controllers/ProjectsController.cs
+++ DevFreela.API/Controllers/ProjectsController.cs
@@ -1,4 +1,5 @@
-﻿using DevFreela.API.Models;
+﻿using DevFreela.Application.InputModels;
+using DevFreela.Application.Services.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DevFreela.API.Controllers
@@ -6,49 +7,59 @@ namespace DevFreela.API.Controllers
     [Route("api/projects")]
     public class ProjectsController : ControllerBase
     {
+        private readonly IProjectService _projectService;
+
+        public ProjectsController(IProjectService projectService)
+        {
+            _projectService = projectService;
+        }
+
         // api/projects?query=net core
         [HttpGet]
         public IActionResult Get(string query)
         {
-            // Buscar todos ou filtrar
+            var projects = _projectService.GetAll(query);
 
-            return Ok ( );
+            return Ok ( projects );
         }
 
         // api/projects/1
         [HttpGet("{id}")]
         public IActionResult GetById(int id)
         {
-            // Buscar o projeto
+            var project = _projectService.GetById(id);
 
-            // return NotFound(); // se não achar
+            if (project == null)
+            {
+                return NotFound ( );
+            }
 
             return Ok ( );
         }
 
         [HttpPost]
-        public IActionResult Post([FromBody] CreateProjectModel createProject)
+        public IActionResult Post([FromBody] NewProjectInputModel inputModel)
         {
-            if(createProject.Title.Length > 50)
+            if(inputModel.Title.Length > 50)
             {
                 return BadRequest ( );
             }
 
-            // Cadastrar o projeto
+            var id = _projectService.Create(inputModel);
 
-            return CreatedAtRoute ( nameof ( GetById ), new { id = createProject.Id }, createProject );
+            return CreatedAtRoute ( nameof ( GetById ), new { id = id }, inputModel );
         }
 
         // api/project/2
         [HttpPut ( "{id}" )]
-        public IActionResult Put ( int id, [FromBody] UpdateProjectModel updateProject )
+        public IActionResult Put ( int id, [FromBody] UpdateProjectInputModel inputModel )
         {
-            if (updateProject.Description.Length > 200)
+            if (inputModel.Description.Length > 200)
             {
                 return BadRequest ( );
             }
 
-            // Atualizo o objeto
+            _projectService.Update(inputModel);
 
             return NoContent ( );
         }
@@ -57,17 +68,17 @@ namespace DevFreela.API.Controllers
         [HttpDelete("{id}")]
         public IActionResult Delete( int id )
         {
-            // Buscar, se não existir, retorna NotFound
-
-            // Remover
+            _projectService.Delelte( id );
 
             return NoContent ( );
         }
 
         // api/projects/1/comments POST
         [HttpPost("{id}/comments")]
-        public IActionResult PostComment( int id, [FromBody] CreateCommentModel createCommentModel )
+        public IActionResult PostComment( int id, [FromBody] CreateCommentInputModel inputModel )
         {
+            _projectService.CreateComment(inputModel);
+
             return NoContent ( );
         }
 
@@ -75,6 +86,8 @@ namespace DevFreela.API.Controllers
         [HttpPut("{id}/start")]
         public IActionResult Start( int id )
         {
+            _projectService.Start(id);
+
             return NoContent ( );
         }
 
@@ -82,6 +95,8 @@ namespace DevFreela.API.Controllers
         [HttpPut("{id}/finish")]
         public IActionResult Finish( int id )
         {
+            _projectService.Finish(id);
+
             return NoContent ( );
         }
     }
