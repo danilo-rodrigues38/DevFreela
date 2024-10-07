e9905761a1207d0dc8b412147d08c3e046f5002e
Danilo Rodrigues de Oliveira <danilo.rodrigues38@gmail.com>
2024-10-07 14:17:14 -0300

    Implementando, configurando e refatorando as entidades para tabelas.

---
+++ DevFreela.Infrastructure/Persistence/Configurations/ProjectCommentConfigurations.cs
@@ -0,0 +1,28 @@
+﻿using DevFreela.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System.Reflection.Emit;
+
+namespace DevFreela.Infrastructure.Persistence.Configurations
+{
+    public class ProjectCommentConfigurations : IEntityTypeConfiguration<ProjectComment>
+    {
+        public void Configure ( EntityTypeBuilder<ProjectComment> builder )
+        {
+            builder
+                .HasKey ( pc => pc.Id );
+
+            builder
+                .HasOne ( pc => pc.Project )
+                .WithMany ( pc => pc.Comments )
+                .HasForeignKey ( pc => pc.IdProject )
+                .OnDelete ( DeleteBehavior.Restrict );
+
+            builder
+                .HasOne ( pc => pc.User )
+                .WithMany ( pc => pc.Comments )
+                .HasForeignKey ( pc => pc.IdUser )
+                .OnDelete ( DeleteBehavior.Restrict );
+        }
+    }
+}
--- DevFreela.Infrastructure/Persistence/DevFreelaDbContext.cs
+++ DevFreela.Infrastructure/Persistence/DevFreelaDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using DevFreela.Core.Entities;
 using Microsoft.EntityFrameworkCore;
+using System.Reflection;
 
 namespace DevFreela.Infrastructure.Persistence
 {
@@ -14,5 +15,10 @@ namespace DevFreela.Infrastructure.Persistence
         public DbSet<Skill> Skills { get; set; }
         public DbSet<UserSkill> UserSkills { get; set; }
         public DbSet<ProjectComment> ProjectComments { get; set; }
+
+        protected override void OnModelCreating ( ModelBuilder modelBuilder )
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly ( Assembly.GetExecutingAssembly ( ) );
+        }
     }
 }
